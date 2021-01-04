2d88796ab33fd9c8e82268f633321cb100a7bbe2
Reinhart Previano K <reinhart_previano@yahoo.com>
2021-01-04 12:01:49 +0700

    feat: Add experimental Cyrillic support

--- scripts/cv-count-latin.sh
+++ scripts/cv-count-latin.sh
@@ -20,7 +20,8 @@ for lang in */ ; do
     cd $lang
     echo $PWD
     # Sort and calculate all words from .txt files (excluding the not-used/ folder)
-    cat raw/*.txt | tr 'A-Z' 'a-z' | tr '\u00c0-\u00d6' '\u00e0-\u00f6' | tr '\u00d8-\u00de' '\u00f8-\u00fe' | sed 's/--/ /g' | sed 's/[^a-z \u00e0-\u00f6\u00f8-\u00fe]//g' | tr -s '[[:space:]]' '\n' | sort | uniq -c | sort -n | tail -n 9999999999 | cat > words.txt
+    #cat raw/*.txt | tr 'A-Z' 'a-z' | tr '\u00c0-\u00d6' '\u00e0-\u00f6' | tr '\u00d8-\u00de' '\u00f8-\u00fe' | sed 's/--/ /g' | sed 's/[^a-z \u00e0-\u00f6\u00f8-\u00fe]//g' | tr -s '[[:space:]]' '\n' | sort | uniq -c | sort -n | tail -n 9999999999 | cat > words.txt
+    cat raw/*.txt | sed 's/[[:upper:]]*/\L&/g' | sed 's/[^[[:lower:]] ]//g' | tr -s '[[:space:]]' '\n' | sort | uniq -c | sort -n | tail -n 9999999999 | cat > words.txt
     # Collect these values to a JSON file
     cat words.txt | sed 's/^[ \t]*//' | sed 's/[ \t]/,/' | cat > words.csv
     cd ../
